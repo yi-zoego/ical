11b812e3487c203b9a0400037962861e31558d06
yi-zoego <yi.zoego@gmail.com>
2026-05-23 15:03:03 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -24,8 +24,8 @@ const IGNORE_COMPANIES = ["新莊", "東認", "北認"];
 const MEMO = [
   "晚_東認：5/14(Word)、6/9(PPT)、7/14(Excel)、7/20(AI24)、8/20(2,4_Word)、9/15(2,4_PPT)",
   "晚_北認 AI：5/29(AI+Word)、6/12(AI+Excel)、7/9(AI生活)",
-  "整_新莊：6/28(AI24)、10/20(AI24)",
   "晚_北認 Office：6/27(六_Excel)、7/6(1,3,5_Excel)、8/6(2,4_PPT)",
+  "整_新莊：6/28(日_AI24)、10/20(2,4_AI24)",
 ];
 
 // https://console.cloud.google.com/apis/credentials/key/682305ca-cf8a-4152-a2fb-6f5c95b149a4?project=ical-zoeclass
